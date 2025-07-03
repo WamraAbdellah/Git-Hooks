e6db4b366fd173857fd3486c359e01a8601ccd54
Abdellah Wamra <146929788+WamraAbdellah@users.noreply.github.com>
2025-07-03 09:20:04 +0100

    update commit-msg-hook.sh

--- commit-msg-hook.sh
+++ commit-msg-hook.sh
@@ -11,13 +11,13 @@ echo "Commit message identified as: $commit_message"
 # Check if the message matches the regex
 echo "$commit_message" | grep -Eq "$conventional_commit_regex"
 if [ $? -eq 0 ]; then
-  echo "✅ Commit message meets Conventional Commit standards."
+  echo "Commit message meets Conventional Commit standards."
   exit 0
 fi
 
 # Not a conventional commit – print error and example
-echo "\033[31m❌ Commit message does not meet the Conventional Commit standard\033[0m"
-echo "👉 Example of a valid message is:"
-echo "   feat(login): add the 'remember me' button"
-echo "🔗 More details at: https://www.conventionalcommits.org/en/v1.0.0/#summary"
+echo "\033[31m Commit message does not meet the Conventional Commit standard\033[0m"
+echo " Example of a valid message is:"
+echo " feat(login): add the 'remember me' button"
+echo " More details at: https://www.conventionalcommits.org/en/v1.0.0/#summary"
 exit 1
